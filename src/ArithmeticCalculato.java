795efbf427690e3f0322be5dd279e7060edf15cc
giwon-wg <giwon8766@naver.com>
2025-02-25 19:19:52 +0900

    LV2. Class 활용

--- src/ArithmeticCalculato.java
+++ src/ArithmeticCalculato.java
@@ -1,15 +1,18 @@
 public class ArithmeticCalculato {
 
     public static int add(int a, int b){
+
         return a + b;
     }
 
-    public static int sub(int a, int b){
+    public static int sub(int a, int b) {
         return a - b;
     }
+
     public static int mul(int a, int b){
         return a * b;
     }
+
     public static double div(int a, int b){
         if (b == 0){
             throw new Error("분모에는 0이 들어갈 수 없습니다");
