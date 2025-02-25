ecf8eac1ec957efc69373ab2955ff44fb3c0acfc
giwon-wg <giwon8766@naver.com>
2025-02-25 15:45:56 +0900

    Lv1. 완료(양의 정수 입력, 사칙연산 입력, 계산, 연산 오류 예외처리, 반복문 사용 및 무한반복, q입력시 반복종료)

--- src/Main.java
+++ src/Main.java
@@ -4,38 +4,67 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 0; i<100; i++) {
+        int a = 0;
+        int b = 0;
+        int result = 0;
+        String c;
 
-            Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 1; ; i++) {
+            System.out.printf("\n========계산 준비 완료 / 사용량 : %d회==========\n\n", i);
             System.out.println("첫번째 숫자를 입력하세요");
-            int a = sc.nextInt();
-            System.out.println("두번째 숫자를 입력하세요");
-            int b = sc.nextInt();
-            System.out.println("사칙연산 기호를 입렵하세요(+, -, *, /)");
-            String c = sc.next();
+            System.out.print(": ");
+            while (!sc.hasNextInt()){
+                System.out.println("잘못된 값을 입력하였습니다. 다시 입력해주세요");
+                System.out.print(": ");
+                sc.next();
+            }
+            a = sc.nextInt();
 
-            int result = 0;
+            System.out.println("두번째 숫자를 입력하세요");
+            System.out.print(": ");
+            while (!sc.hasNextInt()){
+                System.out.println("잘못된 값을 입력하였습니다. 다시 입력해주세요");
+                System.out.print(": ");
+                sc.next();
+            }
+            b = sc.nextInt();
+
+            System.out.println("사칙연산 기호를 입렵하세요(+, -, *, /)");
+            System.out.print(": ");
+            while(true){
+                c = sc.next();
+                if(c.equals("+") || c.equals("-") || c.equals("*") || c.equals("/")){
+                    break;
+                } else{
+                    System.out.println("잘못된 값을 입력하였습니다. 다시 입력해주세요");
+                    System.out.print(": ");
+                }
+            }
+
+
 
             switch (c) {
                 case "+":
                     result = a + b;
-                    System.out.println(a + c + b + "=" + result);
+                    System.out.println("\n"+ a + c + b + "=" + result + "\n");
                     break;
                 case "-":
                     result = a - b;
-                    System.out.println(a + c + b + "=" + result);
+                    System.out.println("\n"+ a + c + b + "=" + result+ "\n");
                     break;
                 case "*":
                     result = a * b;
-                    System.out.println(a + c + b + "=" + result);
+                    System.out.println("\n"+ a + c + b + "=" + result+ "\n");
                     break;
                 case "/":
                     if(b == 0){
-                    System.out.println("분모에는 0이 들어갈 수 없습니다.");
+                    System.out.println("분모에는 0이 들어갈 수 없습니다."+ "\n");
                     break;
                 }
                     result =  a / b;
-                    System.out.println(a + c + b + "=" + result);
+                    System.out.println("\n"+ a + c + b + "=" + result);
                     break;
 
             }
